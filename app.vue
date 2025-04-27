4e3071cd47cf3e50c067e81a0d4d82fcbf6c1610
Leisbanon <15800349672@163.com>
2025-04-27 20:51:44 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+const count = ref(1);
+console.log("a => ", count.value);
+
+onMounted(() => {
+  console.log("A...");
+});
+
+// console.log("b => ", Proxy);
+</script>
---
+++ server/utils/user.js
@@ -0,0 +1,6 @@
+export const getUserInfo = () => {
+  return {
+    name: "leisbanon",
+    phone: "158****9672",
+  };
+};
